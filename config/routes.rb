dc1b347ec3213ec0550bfdb1bd81e4d4157394c3
John Paul <johnarpaul@gmail.com>
2020-04-16 10:31:42 +0100

    custom controllers

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
-  devise_for :users
+  devise_for :users, controllers: {
+    sessions: 'users/sessions',
+    registrations: 'users/registrations'
+  }
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
